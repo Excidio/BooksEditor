af26da13e157e991a92323cf3fd4280cbe7c1941
Excidio <popov095@gmail.com>
2016-08-15 11:58:12 +0500

    Repository + Domain has been added. Remove bin files.

---
+++ Domain/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BooksEditor.Domain
+{
+    public class Book
+    {
+        public string Header { get; set; }
+
+        public int NumberOfPages { get; set; }
+
+        public string PublishingHouse { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public string ISBN { get; set; }
+
+        //public string Image { get; set; } TODO: Add image
+
+        public IEnumerable<Author> Authors { get; set; }
+    }
+}
